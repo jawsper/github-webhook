73abbe1ed76c63082bdf20890e27380dd472c9be
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-03-22 18:43:13 +0100

    feat: support loading config from .env file

--- webhook/config.py
+++ webhook/config.py
@@ -5,6 +5,9 @@ class Config(BaseSettings):
     github_secret: str
     match_tag: str = "latest"
     command: str
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 config = Config()  # type: ignore
