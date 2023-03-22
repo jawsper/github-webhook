c94b696476b823ae166ee8d5e717b7a5e02316ea
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-03-22 23:33:19 +0100

    feat: process Config.command with shlex.split

--- webhook/config.py
+++ webhook/config.py
@@ -1,13 +1,20 @@
-from pydantic import BaseSettings
+import shlex
 
+from pydantic import BaseSettings
 
 class Config(BaseSettings):
     github_secret: str
     match_tag: str = "latest"
-    command: str
+    command: list[str]
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str):
+            if field_name == "command":
+                return shlex.split(raw_val)
+            return raw_val
 
 
 config = Config()  # type: ignore
